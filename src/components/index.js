a5f94de0fb9a58c4757d7a4fffcd48a540c8453b
v1tti <bielvitti@hotmail.com>
2024-08-15 18:30:51 -0300

    adding project

---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+export { QRCodeGenerator } from "./QRCodeGenerator/QRCodeGenerator.jsx";
+export { IPAddressFinder } from "./IPAddressFinder/IPAddressFinder.jsx";
+export { MovieSearchEngine } from "./MovieSearchEngine/MovieSearchEngine.jsx";
+export { TodoApp } from "./TodoApp/TodoApp.jsx";
+export { QuizApp } from "./QuizzApp/QuizApp.jsx";
+export { LanguageTranslator } from "./LanguageTranslator/LanguageTranslator.jsx";
+export { Login } from "./Login/Login.jsx";
